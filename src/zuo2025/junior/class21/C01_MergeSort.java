95685937f6cd2963aaadceb240fed33c9b644bdc
flycool <flycoolcool@163.com>
2025-03-01 17:19:02 +0800

    merge sort

---
+++ src/zuo2025/junior/class21/C01_MergeSort.java
@@ -0,0 +1,53 @@
+package zuo2025.junior.class21;
+
+public class C01_MergeSort {
+
+    public final int n = 501;
+
+    public int[] help = new int[n];
+
+    public void mergeSort(int[] arr, int l, int r) {
+        if (l == r) {
+            return;
+        }
+        int m = (l + r) / 2;
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+        merge(arr, l, m, r);
+    }
+
+    //非递归
+    public void mergeSort2(int[] arr) {
+        for (int l, m, r, step = 1; step < n; step <<= 1) {
+            l = 0;
+            while (l < n) {
+                m = l + step + 1;
+                // 右侧如果没有
+                if (m + 1 >= n) {
+                    break;
+                }
+                r = Math.min(l + (step << 1) - 1, n - 1);
+                merge(arr, l, m, r);
+                l = r + 1;
+            }
+        }
+    }
+
+    public void merge(int[] arr, int l, int m, int r) {
+        int a = l;
+        int b = m + 1;
+        int i = 0;
+        while (a <= m && b <= r) {
+            help[i++] = arr[a] <= arr[b] ? arr[a++] : arr[b++];
+        }
+        while (a <= m) {
+            help[i++] = arr[a++];
+        }
+        while (b <= r) {
+            help[i++] = arr[b++];
+        }
+        for (i = l; i <= r; i++) {
+            arr[i] = help[i];
+        }
+    }
+}
