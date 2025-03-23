9231af7d798b53cffbb48c8e78e0e16eba7dc32f
flycool <flycoolcool@163.com>
2025-03-23 11:34:07 +0800

    algo nonConstructibleChange

---
+++ src/algo_algorithm/easy/NonConstructibleChange.kt
@@ -0,0 +1,17 @@
+package algo_algorithm.easy
+
+class NonConstructibleChange {
+
+    fun nonConstructibleChange(coins: MutableList<Int>): Int {
+        coins.sorted()
+
+        var change = 0 // 1... change 都能得到
+        for (coin in coins) {
+            if (coin > (change + 1)) {
+                break
+            }
+            change += coin
+        }
+        return change + 1
+    }
+}
