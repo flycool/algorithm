518b909d7eb5539dcf4665ebc966772a643ab005
flycool <flycoolcool@163.com>
2025-03-30 10:35:45 +0800

    algo binarySearch

---
+++ src/algo_algorithm/easy/C20_BinarySearch.kt
@@ -0,0 +1,17 @@
+package algo_algorithm.easy
+
+fun binarySearch(array: List<Int>, target: Int): Int {
+    var l = 0
+    var r = array.size - 1
+    while (l <= Result) {
+        var m = l + (r - l) / 2
+        if (array[m] > target) {
+            r = m - 1
+        } else if (array[m] < target) {
+            l = m + 1
+        } else {
+            return array[m]
+        }
+    }
+    return -1
+}
