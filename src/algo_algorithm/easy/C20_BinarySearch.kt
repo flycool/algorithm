ddb9952bd9a1b9e607f46a0d2c58c62bb30f3d98
flycool <flycoolcool@163.com>
2025-03-31 16:27:18 +0800

    algo fix

--- src/algo_algorithm/easy/C20_BinarySearch.kt
+++ src/algo_algorithm/easy/C20_BinarySearch.kt
@@ -3,7 +3,7 @@ package algo_algorithm.easy
 fun binarySearch(array: List<Int>, target: Int): Int {
     var l = 0
     var r = array.size - 1
-    while (l <= Result) {
+    while (l <= r) {
         var m = l + (r - l) / 2
         if (array[m] > target) {
             r = m - 1
--- src/algo_algorithm/easy/C25_RunLengthEncoding.kt
+++ src/algo_algorithm/easy/C25_RunLengthEncoding.kt
@@ -7,14 +7,20 @@ fun runLengthEncoding(string: String): String {
         if (string[i] == string[i - 1] && length < 9) {
             length += 1
         } else {
-            chars.add(length.toChar())
+            chars.add(length.toString()[0])
             chars.add(string[i - 1])
-            length = 0
+            length = 1
         }
     }
     // handle the last run
-    chars.add(length.toChar())
+    chars.add(length.toString()[0])
     chars.add(string[string.length - 1])
 
     return chars.joinToString("")
+}
+
+fun main() {
+    val s = "AAAAAAAAAAAAAAABBBCCCDD"
+    val ans = runLengthEncoding(s)
+    print(ans) //9A6A3B3C2D
 }
