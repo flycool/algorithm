21f98cfa83f41bf0006e5cfa8473926aa16c027b
flycool <flycoolcool@163.com>
2025-04-11 11:40:41 +0800

    搜索二叉树上寻找两个节点的最近公共祖先

--- src/zuo2025/essential/class37_tree2/C01_LowestCommonAncestor.java
+++ src/zuo2025/essential/class37_tree2/C01_LowestCommonAncestor.java
@@ -29,4 +29,23 @@ public class C01_LowestCommonAncestor {
         }
         return l != null ? l : r;
     }
+
+    // 2. 搜索二叉树上寻找两个节点的最近公共祖先
+    /**
+     * root 从上到下
+     * 如果先遇到p，p就是答案
+     * 如果先遇到q，q就是答案
+     * 如果root在p~q的值之间，不管p和q的大小，只要root在中间，root就是答案
+     * 如果root在p~q的值的左侧，那么root往右移动
+     * 如果root在p~q的值的右侧，那么root往左移动
+     */
+    public TreeNode lowestCommonAncesto2r(TreeNode root, TreeNode p, TreeNode q) {
+        while (root.val != p.val && root.val != q.val) {
+            if (Math.min(p.val, q.val) < root.val && root.val < Math.max(p.val, q.val)) {
+                break;
+            }
+            root = root.val < Math.min(p.val, q.val) ? root.right : root.left;
+        }
+        return root;
+    }
 }
